4f7dd2372205b056f4cd5ec743acfdfa2b8b09e4
Silvio <jalcoser1996@gmail.com>
2021-12-15 21:13:58 -0500

    Componente personalizado botón FAB

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import MapView, { Marker } from 'react-native-maps'
 import { useLocation } from '../hooks/useLocation'
 import { LoadingScreen } from '../pages/LoadingScreen';
+import { Fab } from './Fab';
 
 //Si en un futuro quiero varios marcadors
 interface Props{
@@ -30,8 +31,8 @@ export const Map = ({markers}:Props) => {
                     longitudeDelta: 0.0421,
                 }}
             >
-                <Marker
-            
+                {/* <Marker
+
                     image={require('../assests/custom-marker.png')}
                     coordinate={{
                         latitude:37.7885,
@@ -39,8 +40,17 @@ export const Map = ({markers}:Props) => {
                     }}
                     title='Este es un título'
                     description='Esto es la descripción de un marcador'
-                />
+                /> */}
             </MapView>
+            <Fab
+                    iconName='star-outline'
+                    onPress={()=>console.log('HOLA FAB')}
+                    style={{
+                        position:'absolute',
+                        bottom:20,
+                        right:20,
+                    }}
+                />
             </>
             )
 }
